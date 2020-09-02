5d0b2ffdf2f601026fe4d2ac59c080a0ce081a0a
ivan1krulov <ivan1krulov@gmail.com>
2020-09-02 20:17:57 +0300

    WORK!!!

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,16 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({children}) => {
-return <BlogContext.Provider value={5}>
+const blogPosts = [
+{title: 'Blog Posrt #1'},
+{title: 'Blog Posrt #2'},
+{title: 'Blog Posrt #3'},
+{title: 'Blog Posrt #4'},
+{title: 'Blog Posrt #5'},
+
+];
+
+return <BlogContext.Provider value={blogPosts}>
 {children}
 </BlogContext.Provider>;
 };
