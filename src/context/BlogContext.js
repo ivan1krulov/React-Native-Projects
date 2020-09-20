8faea4ea752b8d5cf08cb351d8c0fc6fbaf69684
ivan1krulov <ivan1krulov@gmail.com>
2020-09-20 19:18:53 +0300

    Update AddBlog

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,17 +7,22 @@ switch (action.type) {
     case 'delete_blogpost':
 return state.filter(blogPost => blogPost.id !== action.payload);
 case 'add_blogpost':
-    return [...state, 
-        {id: Math.floor(Math.random() * 9999),  
-            title: 'Blog Post  ' + state.length + 1}];
+    return [
+        ...state, 
+        {
+            id: Math.floor(Math.random() * 9999),  
+            title: action.payload.title,
+            content: action.payload.content
+        }
+    ];
     default:
         return state;
 }
 };
 
-const addBlogPost = (dispatch) => {
-    return () => {
-    dispatch({type: 'add_blogpost'});
+const addBlogPost = dispatch => {
+    return (title, content) => {
+    dispatch({type: 'add_blogpost', payload: {title, content}});
 };
 };
 const deleteBlogPost = dispatch => {
