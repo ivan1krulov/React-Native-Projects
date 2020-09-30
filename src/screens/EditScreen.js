7b9b2daf563cc4ebe14f9c3681ded573af329092
ivan1krulov <ivan1krulov@gmail.com>
2020-09-30 18:11:26 +0300

    + State

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {Context} from '../context/BlogContext';
+
  const EditScreen = ({ navigation}) => {
+ const { state } = useContext(Context);
+
+ const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id')
+ );
+
+const [title, setTitle] = useState(blogPost.title);
+const [content, setContent] = useState(blogPost.content)
      return (
          <View>
-             <Text>
-           {navigation.getParam('id')}
-             </Text>
+             <Text>Edit Title:</Text>
+             <TextInput value={title} onChangeText={(newTitle) => setTitle(newTitle)} />
          </View>
      );
  };
