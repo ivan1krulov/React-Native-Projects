d226359e1326509e72e7e4333e0d59ded81f268f
ivan1krulov <ivan1krulov@gmail.com>
2020-10-15 04:27:15 +0300

    Completed 1

--- tempData.js
+++ tempData.js
@@ -8,33 +8,73 @@ export default tempData = [
             {
                 title: "Book Flight",
                 completed: true
+            },
+
+            {
+                title: "Book Flight",
+                completed: false
+            },
+
+            {
+                title: "Book Flight",
+                completed: true
             }
         ]
     },
     {
         name: "Work Day",
-        color: "red",
+        color: "#8022D9",
         todos: [
             {
                 title: "Book Flight",
                 completed: false
+            },
+
+            {
+                title: "Book Flight",
+                completed: false
+            },
+
+            {
+                title: "Book Flight",
+                completed: true
             }
         ]
     },
 
     {  name: "Friends",
-    color: "green",
+    color: "#58AE4C",
     todos: [
         {
             title: "Book Flight",
             completed: false
+        },
+
+        {
+            title: "Book Flight",
+            completed: false
+        },
+
+        {
+            title: "Book Flight",
+            completed: true
         }
     ]
 
 },
 {  name: "Birthday",
-color: "black",
+color: "#AE514C",
 todos: [
+    {
+        title: "Book Flight",
+        completed: true
+    },
+
+    {
+        title: "Book Flight",
+        completed: true
+    },
+
     {
         title: "Book Flight",
         completed: true
